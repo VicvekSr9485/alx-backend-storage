f4747081d15ecfb8d2c589b4afd61f67802bf7ee
VicvekSr <Olamideoso65@gmail.com>
2023-01-20 01:53:30 +0000

    more redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,30 +1,103 @@
 #!/usr/bin/env python3
-""" This module writes strings to redis
 """
-from typing import Callable, Optional, Union
+Writing strings to Redis
+"""
 import redis
-import sys
 import uuid
+from typing import Union, Optional, Callable
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    count function calls
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    function call history
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    function to display the history of
+    calls of a particular function
+    """
+    key = method.__qualname__
+    redis_cache = redis.Redis()
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+    inputs = redis_cache.lrange(input_key, 0, -1)
+    outputs = redis_cache.lrange(output_key, 0, -1)
+
+    calls = redis_cache.get(key).decode('utf-8')
+    print("{} was called {} times:".format(key, calls))
+
+    for i, o in zip(inputs, outputs):
+        i = i.decode('utf-8')
+        o = o.decode('utf-8')
+
+        print("{}(*{}) -> {}".format(key, i, o))
+
+
 class Cache:
-    """ class cache """
+    """
+    redis cache
+    """
     def __init__(self) -> None:
-        """ class Cache init method """
+        """
+        store an instance of the
+        redis client
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ stores the input data in Redis using the random key and return the key
+        """
+        a method that takes a data
+        argument and returns a string
         """
         key = str(uuid.uuid4())
+
         self._redis.set(key, data)
+
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """ get method """
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """
+        a get method that take a key
+        string argument and an optional
+        Callable argument named fn.
+        """
         value = self._redis.get(key)
 
         if value is not None:
