fd6378fe8c013edbf8cf400ed0c16f521b1ccb10
VicvekSr <Olamideoso65@gmail.com>
2023-01-20 01:39:59 +0000

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
     def __init__(self) -> None:
         """ class Cache init method """
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
     
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
