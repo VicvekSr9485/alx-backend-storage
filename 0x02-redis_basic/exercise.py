bd2ad262730d8b1780ac9c8c6fe5c1a9d0268afe
VicvekSr <Olamideoso65@gmail.com>
2023-01-20 01:49:07 +0000

    more redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,3 +22,30 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get method """
+        value = self._redis.get(key)
+
+        if value is not None:
+            if fn:
+                value = fn(value)
+            return value
+        else:
+            return None
+
+    def get_str(self, val: bytes) -> str:
+        """
+        automatically parametrize Cache.get
+        returns a string
+        """
+        return str(val, val.decode('utf-8'))
+
+    def get_int(self, val: bytes) -> int:
+        """
+        automatically parametrize Cache.get
+        returns an int
+        """
+        val = int(val, val.decode('utf-8'))
+
+        return val if val else 0
